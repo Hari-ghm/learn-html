5a0ba3241266d4f54021ed5fa4ca784bec7683e4
Hari-ghm <hmadhav2006@gmail.com>
2024-09-26 22:25:31 +0530

    done till variables

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log("Hello");
+// This is single line comment in js. in js, ; is not mandatory at end of eack line.
+
+/* this is a multi line comment
+if u have set of code of n lines and want to single comment everyline, use crtl + / 
+*/
+
+// variable declaration
+let book_Name; // let is a keyword to declare variables.space and spl chr(except _ and $) is not allowed in variable.
+book_Name='harry potter'; // variables are case sensitive
+
+let book_authors='jkr',publ_yr=1996;
+console.log("book Name: ",book_Name,'\n',"book authon: ",book_authors,'\n',"yr published: ",publ_yr);
+
+publ_yr=2000;// dynamic typing is allowed in js
+console.log(publ_yr)
+
+
+
+
+
